36e39eddaac0bc8bc64469907df0f870a52fa2fa
KlintLee115 <programmerder@gmail.com>
2022-12-10 16:46:47 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var xValues = [-3, -2, -1, 0, 1, 2, 3];
+
+var arocChart = new Chart(document.getElementById('arocChart'), {
+    type: "bar",
+    data: {
+        labels: xValues,
+        datasets: [{
+            label: 'y=x^2',
+            type: 'line',
+            pointRadius: 5,
+            borderColor: "cyan",
+            borderWidth: 2,
+            data: [9, 4, 1, 0, 1, 4, 9],
+            backgroundColor: "cyan",
+            fill: false,
+            tension: 0.4,
+            pointBackgroundColor: "blue",
+        }, {
+            label: 'secant/aroc',
+            type: 'line',
+            pointRadius: 4,
+            data: [null, null, null, null, 1, 4, null],
+            borderColor: 'orange',
+            borderWidth: 2,
+            fill: false,
+        }]
+    }
+});
